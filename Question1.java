b8074f46ba100f7e3c9a28243357cdacfa82ed3d
MANTHIPERERA <93880555+MANTHIPERERA@users.noreply.github.com>
2022-02-16 23:56:20 +0530

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,17 @@
+public class Question1{
+	public static void main(String[] args) {
+		// add up the numbers
+		int count = 0;
+
+		
+		do {
+			System.out.print(" Hello "); 
+			count = count + 1;
+		} 
+
+		while (count <= 9);
+
+		System.out.println("Quiting program..");
+
+	}
+}
